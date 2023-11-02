5fa1f0f366867c03cbce64558416d567f2a9fe7f
Yuanzth <adityayuhanda3001@gmail.com>
2023-11-02 14:26:04 +0700

    Percobaan 1 Nomor 12

--- Bioskop02.java
+++ Bioskop02.java
@@ -25,12 +25,10 @@ public static void main(String[] args) {
         System.out.println("Panjang baris : " + barisPenonton.length );
     }
 
-    System.out.println("Penonton pada baris ke-3: ");
-    for (String i : penonton[2])
+    for (int i = 0; i < penonton.length; i++)
     {
-        System.out.println(i);
+        System.out.println("Penonton pada baris ke-"+(i+1)+": " + String.join(", ", penonton[i]));
     }
-
 }
     
 }
