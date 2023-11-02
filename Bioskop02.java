0e827a5c1f7e69be07c72f7ca1cd8d1d19d7fe92
Yuanzth <adityayuhanda3001@gmail.com>
2023-11-02 14:23:09 +0700

    Percobaan 1 Nomor 11

--- Bioskop02.java
+++ Bioskop02.java
@@ -12,17 +12,25 @@ public static void main(String[] args) {
     penonton[2][1] = "Farhan";
     penonton[3][0] = "Gisel";
     penonton[3][1] = "Hana";
-    
+
     System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
     System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
     System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
     System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
     System.out.println(penonton.length);
-    for (int i = 0; i < penonton.length; i++)
+
+    for (String[] barisPenonton : penonton)
     {
-        System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        System.out.println("Panjang baris : " + barisPenonton.length );
     }
+
+    System.out.println("Penonton pada baris ke-3: ");
+    for (String i : penonton[2])
+    {
+        System.out.println(i);
+    }
+
 }
     
 }
